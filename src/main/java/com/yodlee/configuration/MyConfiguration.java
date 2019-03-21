91cee8fee7b8c87b0342333dd9650dedacff11db
Gowtham Behara <gothsiddu@gmail.com>
2019-03-21 16:18:30 +0530

    Define method to get service obj

--- src/main/java/com/yodlee/configuration/MyConfiguration.java
+++ src/main/java/com/yodlee/configuration/MyConfiguration.java
@@ -1,10 +1,15 @@
 package com.yodlee.configuration;
 
+import com.yodlee.service.MyService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(value = {"com.yodlee.consumer"})
 public class MyConfiguration {
-
+    @Bean
+    public MyService getMyService() {
+        return new MyService();
+    }
 }
