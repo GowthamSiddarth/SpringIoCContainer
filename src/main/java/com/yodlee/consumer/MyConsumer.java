0a567e40114b57c0c83c2770601501b0a08b9b68
Gowtham Behara <gothsiddu@gmail.com>
2019-03-21 16:18:48 +0530

    Define method to set service with reference

--- src/main/java/com/yodlee/consumer/MyConsumer.java
+++ src/main/java/com/yodlee/consumer/MyConsumer.java
@@ -1,11 +1,17 @@
 package com.yodlee.consumer;
 
 import com.yodlee.service.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyConsumer {
     private MyService myService;
+
+    @Autowired
+    public void setMyService(MyService myService) {
+        this.myService = myService;
+    }
 
     public void log(String message) {
         myService.log(message);
